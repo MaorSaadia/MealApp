611f870f4ce595156613747854e26aff698ef32d
Maor <maorsa9@gmail.com>
2023-07-28 15:09:04 +0300

    Working With CategoriesScreen

---
+++ components/CategoryGridTitle.js
@@ -0,0 +1,44 @@
+import { View, Pressable, Text, StyleSheet } from 'react-native';
+
+function CategoryGridTitle({ title, color }) {
+  return (
+    <View style={[styles.gridItem, { backgroundColor: color }]}>
+      <Pressable android_ripple={{ color: '#fbfefe' }} style={styles.button}>
+        <View style={[styles.innerContainer]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default CategoryGridTitle;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    //Shadow For Android
+    elevation: 4,
+    overflow: 'hidden',
+  },
+  button: {
+    flex: 1,
+  },
+  //   buttonPressed:{
+  //     opacity:0.5
+  //   },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row-reverse',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 17,
+  },
+});
